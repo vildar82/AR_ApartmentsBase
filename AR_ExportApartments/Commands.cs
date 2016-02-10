9a55ea2f482b633fee6e46731eb8003c0716c067
Vildar Khisyametdinov <vildar82@gmail.com>
2016-02-10 17:46:34 +0300

    нетранслируемые файлы!?

---
+++ AR_ExportApartments/Commands.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcadLib.Errors;
+using AR_ApartmentExport.Model.ExportBlocks;
+using AR_ExportApartments.Model.ExportApartment;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: CommandClass(typeof(AR_ExportApartments.Commands))]
+
+namespace AR_ExportApartments
+{
+   public class Commands
+   {
+      [CommandMethod("PIK", "AR-ExportApartments", CommandFlags.Modal | CommandFlags.NoPaperSpace | CommandFlags.NoBlockEditor)]
+      public void ExportApartments()
+      {
+         Logger.Log.Info("Start command AR-ExportApartments");
+         Document doc = Application.DocumentManager.MdiActiveDocument;
+         if (doc == null) return;
+
+         try
+         {
+            Inspector.Clear();
+
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+
+            // Считывание выбранных блоков
+            var blocksApartment = BlockApartment.GetBlockApartments(db);
+            if (blocksApartment.Count == 0)
+            {
+               throw new System.Exception($"Блоки квартир не найдены. Имя блока квартиры должно соответствовать условию Match = '{Options.Instance.BlockApartmentNameMatch}'");
+            }
+
+            ed.WriteMessage($"\nВ Модели найдено {blocksApartment.Count} блоков квартир. ");
+
+            // Форма предпросмотра экспорта блоков
+            FormBlocksExport formExport = new FormBlocksExport(blocksApartment);
+            if (Application.ShowModalDialog(formExport) == System.Windows.Forms.DialogResult.OK)
+            {
+               // Экспорт блоков в файлы
+               var count = BlockApartment.ExportToFiles(blocksApartment);
+
+               ed.WriteMessage($"\nЭкспортированно {count} блоков.");
+
+               // Запись лога экспортированных блоков      
+               string logFile = Path.Combine(Path.GetDirectoryName(doc.Name), Options.Instance.LogFileName);
+               ExcelLog excelLog = new ExcelLog(logFile);
+               try
+               {
+                  excelLog.AddtoLog(blocksApartment);
+               }
+               catch (System.Exception ex)
+               {
+                  Inspector.AddError($"Ошибка записи экспорта в лог файл {logFile} - {ex.Message}");
+               }
+
+               if (Inspector.HasErrors)
+               {
+                  Inspector.Show();
+               }
+            }
+         }
+         catch (System.Exception ex)
+         {
+            doc.Editor.WriteMessage($"\nОшибка экспорта блоков: {ex.Message}");
+            if (!ex.Message.Contains("Отменено пользователем"))
+            {
+               Logger.Log.Error(ex, $"Command: AR-ExportApartments. {doc.Name}");
+            }
+         }         
+      }
+   }
+}
---
+++ AR_ExportApartments/Model/Options.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Xml.Serialization;
+using AcadLib.Files;
+using Autodesk.AutoCAD.ApplicationServices;
+
+namespace AR_ExportApartments
+{
+   [Serializable]
+   public class Options
+   {
+      private static readonly string fileOptions = Path.Combine(
+                     AutoCAD_PIK_Manager.Settings.PikSettings.ServerShareSettingsFolder,
+                     "АР\\ExportApartment\\AR_ExportApartment_Options.xml");
+      private static Options _instance;
+      public static Options Instance
+      {
+         get
+         {
+            if (_instance == null)
+            {
+               _instance = Load();
+            }
+            return _instance;
+         }
+      }
+
+      private Options() {}
+
+      public string LogFileName { get; set; }= "AR_ExportApartment_Log.xlsx";
+      public string BlockApartmentNameMatch { get; set; } = "^(RV_FL|RV_MD)";
+
+      public static Options Load()
+      {
+         Options options = null;
+         // загрузка из файла настроек
+         if (File.Exists(fileOptions))
+         {
+            SerializerXml xmlSer = new SerializerXml(fileOptions);
+            try
+            {
+               options = xmlSer.DeserializeXmlFile<Options>();
+               if (options != null)
+               {  
+                  return options;
+               }
+            }
+            catch (Exception ex)
+            {
+               Logger.Log.Error(ex, "Не удалось десериализовать настройки из файла {0}", fileOptions);
+            }
+         }
+         options = new Options();
+         options.Save();
+         return options;
+      }     
+
+      public void Save()
+      {
+         try
+         {
+            if (!File.Exists(fileOptions))
+            {
+               Directory.CreateDirectory(Path.GetDirectoryName(fileOptions));
+            }
+            SerializerXml xmlSer = new SerializerXml(fileOptions);
+            xmlSer.SerializeList(this);            
+         }
+         catch (Exception ex)
+         {
+            Logger.Log.Error(ex, "Не удалось сериализовать настройки в {0}", fileOptions);
+         }
+      }      
+
+      //private static Options DefaultOptions()
+      //{
+      //   Options options = new Options();
+
+      //   options.LogFileName = "AR_ExportApartment_Log.xlsx";
+      //   options.BlockApartmentNameMatch = "квартира";
+
+      //   return options;
+      //}      
+   }
+}
