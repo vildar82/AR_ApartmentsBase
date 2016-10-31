2694ab2841eecc897ebc6af7d1b0cfd387eb93d3
Vildar Khisyametdinov <vildar82@gmail.com>
2016-10-31 10:10:07 +0300

    DBElement - конкретные типы

--- AR_ApartmentsBase/Model/Elements/Element.cs
+++ AR_ApartmentsBase/Model/Elements/Element.cs
@@ -17,8 +17,8 @@ namespace AR_ApartmentBase.Model.Elements
         public string FamilyName { get; set; }
         public string FamilySymbolName { get; set; }
         public string CategoryElement { get; set; }
-        public object DBElement { get; set; }
-        public object DBElementInApart { get; set; }
+        public F_S_Elements DBElement { get; set; }
+        public F_nn_Elements_Modules DBElementInApart { get; set; }
         public string Name { get; set; }
         public List<Parameter> Parameters { get; set; }
         public string Direction { get; set; }
--- AR_ApartmentsBase/Model/Elements/IElement.cs
+++ AR_ApartmentsBase/Model/Elements/IElement.cs
@@ -1,15 +1,17 @@
 ﻿using AR_ApartmentBase.Model;
 using System;
 using System.Collections.Generic;
+using AR_ApartmentBase.Model.DB.EntityModel;
+
 public interface IElement : IEquatable<IElement>
 {
     string CategoryElement { get; set; }
-    object DBElement { get; set; }
-    object DBElementInApart { get; set; }
+    F_S_Elements DBElement { get; set; }
+    F_nn_Elements_Modules DBElementInApart { get; set; }
     string Direction { get; set; }
     string FamilyName { get; set; }
     string FamilySymbolName { get; set; }
     string LocationPoint { get; set; }
     string Name { get; set; }
-    List<Parameter> Parameters { get; set; }
+    List<Parameter> Parameters { get; set; }    
 }
